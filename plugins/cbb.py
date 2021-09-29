2300f0931b5e7b84ed6614a8ee9872f37f12d3ca
Kuttisrank <87353723+Kuttisrank@users.noreply.github.com>
2021-09-29 12:26:50 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/Bruce_Lee_2X2'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ GROUP : <a href='https://t.me/joinchat/jyp_-d874eQzMmZl'>Click here</a>\n○ Series Channel : @series_drawer\n○ Pdisk channel : <a href='https://t.me/joinchat/SuBbc27an7A4NmFl'>Click here</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/Bruce_Lee_2X2'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ GROUP : <a href='https://t.me/joinchat/jyp_-d874eQzMmZl'>Click here</a>\n○ Series Channel : @series_drawer\n○ Pdisk channel : <a href='https://t.me/joinchat/SuBbc27an7A4NmFl'>Click here</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
